cac56940224f6c1f79bee9faa381156f1e692af3
a-she <al@alswebdesigns.net>
2015-12-20 09:31:43 -0800

    Misc. code clean up

--- index.js
+++ index.js
@@ -2,17 +2,12 @@ var express = require('express');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
-
-//orig: app.use(express.static(__dirname + '/public'));
 app.use(express.static(__dirname + '/'));
 
-// views is directory for all template files
-//app.set('views', __dirname + '/views');
+// Simple version: Set view dir to webroot; put index in webroot
 app.set('views', __dirname + '/');
 app.set('view engine', 'ejs');
-
 app.get('/', function(request, response) {
-//response.render('pages/index');
   response.render('index');
 });
 
